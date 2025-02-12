9935c49529ce38207a6e9f7c8034f0a7119f2470
HardBoss07 <matteo@bosshard.net>
2025-02-12 10:56:44 +0100

    added letter that pops up when clicking on the heart

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     const heart = document.querySelector(".heart");
     const arrow = document.querySelector(".arrow");
+    const paper = document.querySelector(".paper");
+    const letter = document.querySelector(".paper .letter");
 
     function shootArrow() {
         arrow.classList.add("animate-arrow");
         
         setTimeout(() => {
             heart.classList.add("scale-heart");
-            arrow.classList.add("scale-arrow");
         }, 1000);
+        setTimeout(() => {
+            paper.style.opacity = 1;
+            paper.style.visibility = "visible";
+        }, 2000);
+
+        setTimeout(() => {
+            letter.style.opacity = 1;
+        }, 3000);
 
         heart.removeEventListener("click", shootArrow);
     }
